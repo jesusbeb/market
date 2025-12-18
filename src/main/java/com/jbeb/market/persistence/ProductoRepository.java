ae8fc82f37b7178f29f5d800f352c9943723aa12
jesusbeb <jesusbelb@gmail.com>
2025-12-17 18:39:54 -0600

    Repositorio de Productos Orientado al Dominio

--- src/main/java/com/jbeb/market/persistence/ProductoRepository.java
+++ src/main/java/com/jbeb/market/persistence/ProductoRepository.java
@@ -1,38 +1,57 @@
 package com.jbeb.market.persistence;
 
+import com.jbeb.market.domain.Product;
+import com.jbeb.market.domain.repository.ProductRepository;
 import com.jbeb.market.persistence.crud.ProductoCrudRepository;
 import com.jbeb.market.persistence.entity.Producto;
+import com.jbeb.market.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+// ProductoRepository lo orientamos al dominio (Product) implementando de ProductRepository
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository{
     private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
 
     // finAll() retorna una Iterable, por eso lo casteamos a List
-    public List<Producto> getAll(){
-        return (List<Producto>) productoCrudRepository.findAll();
+    // List<Producto> lo mapeamos a List<Product> con el metodo toProducts de mapper
+    @Override
+    public List<Product> getAll(){
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
     }
 
-    public List<Producto> getByCategoria(int idCategoria){
-        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId){
+        List<Product> products = mapper.toProducts( productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId) );
+        return Optional.of(products);
     }
 
-    public Optional<List<Producto>> getEscasos(int cantidad, boolean estado){
-        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    // Como no se tiene un mapeador que convierta de Optional<List<Producto>> a Optional<List<Product>> hacemos map a
+    // List<Producto> y con toProducts() convertimos a List<Product>, ya con esto map retorna por default un List<Optional>
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity){
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> mapper.toProducts(prods));
     }
 
-    public Optional<Producto> getProducto(int id){
-        return productoCrudRepository.findById(id);
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map( prod -> mapper.toProduct(prod));
     }
 
-    public Producto save(Producto producto){
-        return productoCrudRepository.save(producto);
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(producto);
     }
 
-    public void delete(int idProducto){
-        productoCrudRepository.deleteById(idProducto);
+    @Override
+    public void delete(int productId) {
+        productoCrudRepository.deleteById(productId);
     }
+
 }
